ea9650aeb4baaa59789a39a7b1a2d434bc342246
Das Satbir Singh <dsatbir1@gmail.com>
2022-09-18 21:36:25 +0530

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 
-
+app.post('/',function(req,res){
+    return res.json(200,{
+        message:`hello`
+    })
+})
 
 app.listen(8000,function(err){
     if(err) throw err;
